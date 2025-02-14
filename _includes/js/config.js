6aba03a7644327b9086a21d2a32fd3c268e81dd0
André Heughebaert <a.heughebaert@biodiversity.be>
2025-02-14 11:46:44 +0100

    fixing Data Filtering issue#29

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -1,110 +1,139 @@
 var primaryColor = themeStyle && themeStyle.colors && themeStyle.colors.primary;
 var isSquared = themeStyle && themeStyle.square;
 
-const countryCode = 'BE';
+const countryCode = "BE";
 
 if (primaryColor) {
-  var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', extendWith: {
-    primary: primaryColor,
-    borderRadius: isSquared? 0 : 3
-  }});
+  var siteTheme = gbifReactComponents.themeBuilder.extend({
+    baseTheme: "light",
+    extendWith: {
+      primary: primaryColor,
+      borderRadius: isSquared ? 0 : 3,
+    },
+  });
 }
 
 var siteConfig = {
   version: 2,
-  availableCatalogues: ['OCCURRENCE', 'DATASET', 'PUBLISHER', 'LITERATURE'],
+  availableCatalogues: ["OCCURRENCE", "DATASET", "PUBLISHER", "LITERATURE"],
   routes: {
-    enabledRoutes: ['occurrenceSearch', 'publisherSearch', 'publisherKey', 'datasetKey', 'datasetSearch', 'literatureSearch', 'collectionSearch'],
+    enabledRoutes: [
+      "occurrenceSearch",
+      "publisherSearch",
+      "publisherKey",
+      "datasetKey",
+      "datasetSearch",
+      "literatureSearch",
+      "collectionSearch",
+    ],
     alwaysUseHrefs: true,
   },
   occurrence: {
     mapSettings: {
       lat: 50.6,
       lng: 4.3,
-      zoom: 8.4
+      zoom: 8.4,
     },
     rootPredicate: {
-      "type": "or",
-      "predicates": [
-        // { // it is possible to include records published from Belgium
-        //   "key": "publishingCountry",
-        //   "type": "equals",
-        //   "value": countryCode
-        // },
+      type: "and",
+      predicates: [
         {
-          "type": "and",
-          "predicates": [
+          // it is possible to include records published from Belgium
+          key: "notIssues",
+          type: "equals",
+          value: "COUNTRY_COORDINATE_MISMATCH",
+        },
+        {
+          type: "or",
+          predicates: [
             {
-              "key": "country",
-              "type": "equals",
-              "value": countryCode
+              key: "country",
+              type: "equals",
+              value: countryCode,
             },
-            { // exclude records that have coordinate issues
-              "key": "notIssues",
-              "type": "equals",
-              "value": "COUNTRY_COORDINATE_MISMATCH"
-            }
-          ]
-        }
-      ]
+            {
+              // exclude records that have coordinate issues
+              key: "publishingCountry",
+              type: "equals",
+              value: countryCode,
+            },
+          ],
+        },
+      ],
     },
-    highlightedFilters: ['taxonKey', 'gadmGid', 'stateProvince', 'publisherKey', 'elevation', 'year', 'basisOfRecord', 'datasetName', 'occurrenceIssue'],
+    highlightedFilters: [
+      "taxonKey",
+      "gadmGid",
+      "stateProvince",
+      "publisherKey",
+      "elevation",
+      "year",
+      "basisOfRecord",
+      "datasetName",
+      "occurrenceIssue",
+    ],
     // occurrenceSearchTabs: ['MAP', 'TABLE', 'GALLERY', 'DATASETS'] // what tabs should be shown
     // see https://hp-theme.gbif-staging.org/data-exploration-config for more options
   },
   dataset: {
-    rootFilter: {publishingCountry: countryCode},
-    highlightedFilters: ['q', 'anyPublisherKey', 'datasetType', 'license'],
-    excludedFilters: ['publishingCountryCode'],
+    rootFilter: { publishingCountry: countryCode },
+    highlightedFilters: ["q", "anyPublisherKey", "datasetType", "license"],
+    excludedFilters: ["publishingCountryCode"],
   },
   publisher: {
-    rootFilter: {country: countryCode},
-    excludedFilters: ['countrySingle', 'networkKey'],
+    rootFilter: { country: countryCode },
+    excludedFilters: ["countrySingle", "networkKey"],
   },
   collection: {
-    excludedFilters: ['countryGrSciColl'],
+    excludedFilters: ["countryGrSciColl"],
     rootFilter: {
       displayOnNHCPortal: true,
       country: countryCode,
-	  active: true
-    }
+      active: true,
+    },
   },
   institution: {
-    excludedFilters: ['countryGrSciColl'],
+    excludedFilters: ["countryGrSciColl"],
     rootFilter: {
       displayOnNHCPortal: true,
       country: countryCode,
-      active: true
+      active: true,
     },
     mapSettings: {
       enabled: true,
       lat: 50.6,
       lng: 4.3,
-      zoom: 8.4
+      zoom: 8.4,
     },
   },
   literature: {
     rootFilter: {
       predicate: {
-        type: 'or', predicates: [
+        type: "or",
+        predicates: [
           {
-            type: 'in',
-            key: 'countriesOfResearcher',
-            values: [countryCode]
+            type: "in",
+            key: "countriesOfResearcher",
+            values: [countryCode],
           },
           {
-            type: 'in',
-            key: 'countriesOfCoverage',
-            values: [countryCode]
-          }
-        ]
-      }
+            type: "in",
+            key: "countriesOfCoverage",
+            values: [countryCode],
+          },
+        ],
+      },
     },
-    highlightedFilters: ['q', 'countriesOfResearcher', 'countriesOfCoverage', 'year']
+    highlightedFilters: [
+      "q",
+      "countriesOfResearcher",
+      "countriesOfCoverage",
+      "year",
+    ],
   },
   maps: {
-    locale: 'fr'
-  }
+    locale: "fr",
+  },
 };
 
 // example of a language specific route overwrite
